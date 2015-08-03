050cd6fafa195c94534c8a364c8ff2244bfdd9a6
Matt Juszczak <mjuszczak@bitlancer.com>
2015-08-03 13:47:19 -0400

    replacing attribute namespace

--- recipes/agent_windows.rb
+++ recipes/agent_windows.rb
@@ -1,15 +1,15 @@
 
-server_ip = node['go']['agent']['server_host']
-install_path = node['go']['agent']['install_path']
+server_ip = node['gocd']['agent']['server_host']
+install_path = node['gocd']['agent']['install_path']
 
 opts = ""
-opts << "/SERVERIP=#{server_ip} " if node['go']['agent']['server_host']
-opts << "/D=#{install_path}" if node['go']['agent']['install_path']
+opts << "/SERVERIP=#{server_ip} " if node['gocd']['agent']['server_host']
+opts << "/D=#{install_path}" if node['gocd']['agent']['install_path']
 
-download_url = node['go']['agent']['download_url']
+download_url = node['gocd']['agent']['download_url']
 if !download_url
-  major_ver = node['go']['version'].split('-', 2).first
-  download_url = "http://download01.thoughtworks.com/go/#{major_ver}/ga/go-agent-#{node['go']['version']}-setup.exe"
+  major_ver = node['gocd']['version'].split('-', 2).first
+  download_url = "http://download01.thoughtworks.com/go/#{major_ver}/ga/go-agent-#{node['gocd']['version']}-setup.exe"
 end
 
 windows_package 'Go Agent' do
